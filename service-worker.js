1b4b218a740e5c345a5904f56e2d59509c20cd82
ezzatron <contact@erin.id.au>
2023-05-21 07:57:41 +0000

    Deploying to gh-pages from @ ghalactic/branding@4c806525f33d2ece866584329b1af476779255b2 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open("cache-1684238908239")
+    caches.open("cache-1684655832877")
       .then(cache => cache.add('.'))
   )
 })
